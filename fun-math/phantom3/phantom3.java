ce7f99173535f95067c295092d531140a863bf15
Justin Su <3387175+injust@users.noreply.github.com>
2017-11-28 16:25:59 -0500

    Solve phantom3

---
+++ fun-math/phantom3/phantom3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.BitSet;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(in.readLine());
+		long m = Long.parseLong(in.readLine());
+		in.close();
+		BitSet sieve = new BitSet(20000000);
+		int size = (int) Math.ceil(Math.sqrt(m));
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i < size; i++) {
+			if (!sieve.get(i)) {
+				primes.add(i);
+				for (int j = i; j < size; j += i) {
+					sieve.set(j);
+				}
+			}
+		}
+		sieve.clear();
+		int min;
+		int out = 0;
+		for (int prime : primes) {
+			min = (int) (n / prime * prime - n);
+			if (min < 0) {
+				min += prime;
+			}
+			for (; min < m - n; min += prime) {
+				sieve.set(min);
+			}
+			if (prime >= n) {
+				out++;
+			}
+		}
+		System.out.println(out + m - n - sieve.cardinality());
+	}
+}
