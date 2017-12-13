8c244387874aa97b94125568bec7dea23d140013
Justin Su <3387175+injust@users.noreply.github.com>
2017-12-13 12:52:29 -0500

    Use class implementation of a BIT

--- dmopc/dmopc14c2p6/dmopc14c2p6.py
+++ dmopc/dmopc14c2p6/dmopc14c2p6.py
@@ -1,24 +1,28 @@
-def query(ind):
-	ret = 0
-	while ind:
-		ret += bit[ind]
-		ind -= -ind & ind
-	return ret
+class BIT:
+	def __init__(self, length):
+		self.data = [0] * (length + 1)
+		self.length = length
 
+	def query(self, ind):
+		ret = 0
+		while ind:
+			ret += self.data[ind]
+			ind -= -ind & ind
+		return ret
 
-def update(ind, delta):
-	while ind <= n:
-		bit[ind] += delta
-		ind += -ind & ind
+	def update(self, ind, delta):
+		while ind <= self.length:
+			self.data[ind] += delta
+			ind += -ind & ind
 
 
 input = __import__("sys").stdin.readline
 n = int(input())
-bit = [0] * (n + 1)
+bit = BIT(n)
 trees = sorted(enumerate(map(int, input().split()), start=1), key=lambda _: _[1])
 out = [0] * int(input())
 for pos, line in sorted(enumerate(map(int, input().split()) for _ in xrange(len(out))), key=lambda _: -_[1][2]):
 	while trees and trees[-1][1] >= line[2]:
-		update(*trees.pop())
-	out[pos] = str(query(line[1] + 1) - query(line[0]))
+		bit.update(*trees.pop())
+	out[pos] = str(bit.query(line[1] + 1) - bit.query(line[0]))
 print("\n".join(out))
