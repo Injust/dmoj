970ebee93c44f9ab6ceb8aaef8b974a873a6ceaf
Justin Su <3387175+injust@users.noreply.github.com>
2018-01-04 20:02:41 -0500

    Solve cco15p4

---
+++ cco/cco15p4/cco15p4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+char grid[2001][2001];
+int gx, gy;
+
+void dfs(int x, int y) {
+	if (grid[y][x] == 'N') {
+		for (int yy = y - 1; yy >= 0; yy--) {
+			if (grid[yy][x] != '.') {
+				dfs(x, yy);
+			}
+		}
+	} else if (grid[y][x] == 'S') {
+		for (int yy = y + 1; yy < gy; yy++) {
+			if (grid[yy][x] != '.') {
+				dfs(x, yy);
+			}
+		}
+	} else if (grid[y][x] == 'E') {
+		for (int xx = x + 1; xx < gx; xx++) {
+			if (grid[y][xx] != '.') {
+				dfs(xx, y);
+			}
+		}
+	} else if (grid[y][x] == 'W') {
+		for (int xx = x - 1; xx >= 0; xx--) {
+			if (grid[y][xx] != '.') {
+				dfs(xx, y);
+			}
+		}
+	}
+	grid[y][x] = '.';
+	printf("(%d,%d)\n", y, x);
+}
+
+int main() {
+	scanf("%d %d", &gy, &gx);
+	for (int y = 0; y < gy; y++) {
+		scanf("%s", grid[y]);
+	}
+	for (int y = 0; y < gy; y++) {
+		for (int x = 0; x < gx; x++) {
+			if (grid[y][x] != '.') {
+				dfs(x, y);
+			}
+		}
+	}
+	return 0;
+}
