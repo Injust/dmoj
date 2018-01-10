92e689df65021e1c0311ec2fb9fc6753ac878cbf
Justin Su <3387175+injust@users.noreply.github.com>
2018-01-10 05:02:53 -0500

    Update factorial2 generator

--- fun-math/factorial2/generator.py
+++ fun-math/factorial2/generator.py
@@ -1,19 +1,18 @@
-def factorial(start, value, target):
-	for i in range(start + 1, target + 1):
-		value = value * i % 4294967291
-	return value
+def factorial(left, right, val):
+	for i in xrange(left + 1, right + 1):
+		val = val * i % 4294967291
+	return val
 
 
 cycle = 1
 interval = int(__import__("sys").argv[1])
-temp = 1
-value = 1
-for i in range(interval, 4294967291 + interval * 7, interval):
-	value = factorial(i - interval, value, i)
-	temp = temp << 32 | value
+out = 1
+val = 1
+for i in xrange(interval, 4294967291 + interval * 7, interval):
 	cycle += 1
+	val = factorial(i - interval, i, val)
+	out = out << 32 | val
 	if cycle == 5:
-		for shift in range(0, 160, 20):
-			__import__("sys").stdout.write(unichr(0x10000 + (temp >> shift & 0x100000 - 1)).encode("utf-8"))
+		__import__("sys").stdout.write("".join(unichr(0x10000 + (out >> shift & 0x100000 - 1)) for shift in xrange(0, 160, 20)).encode("utf-8"))
 		cycle = 0
-		temp = 0
+		out = 0
