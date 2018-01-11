c590e44e0301fbdc6991ddcae980d93a1b1900dc
Justin Su <3387175+injust@users.noreply.github.com>
2018-01-11 13:04:24 -0500

    Rebase constants in factorial2 generator

--- fun-math/factorial2/generator.py
+++ fun-math/factorial2/generator.py
@@ -13,6 +13,6 @@ for i in xrange(interval, 4294967291 + interval * 7, interval):
 	val = factorial(i - interval, i, val)
 	out = out << 32 | val
 	if cycle == 5:
-		__import__("sys").stdout.write("".join(unichr(0x10000 + (out >> shift & 0x100000 - 1)) for shift in xrange(0, 160, 20)).encode("utf-8"))
+		__import__("sys").stdout.write("".join(unichr(65536 + (out >> shift & 1048575)) for shift in xrange(0, 160, 20)).encode("utf-8"))
 		cycle = 0
 		out = 0
