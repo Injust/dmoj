9bd35b42e5d0f8eed9fc6fc62e6eb554b4b89fc3
Justin Su <3387175+injust@users.noreply.github.com>
2018-04-06 10:07:26 -0400

    A lot of progress

--- fun-math/phantomc1/phantomc1.py
+++ fun-math/phantomc1/phantomc1.py
@@ -1,1 +1,1 @@
-print(lambda p:" ".join(([p.__setitem__(j,0)for j in range(i*i,len(p),i)],`i`+"*"*(p[i-2]|p[i+2]))[1]for i in range(2,len(p))if p[i]))([0]+[1]*input())
+print(lambda p:" ".join(([p.__setitem__(j,0)for j in p[i*i::i]],`i`+"*"*(p[i-2]|p[i+2]>1))[1]for i in p[2:]if p[i]))(range(input()))
