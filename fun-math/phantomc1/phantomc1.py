b5f15516f4e19ca578d1f4cc4fe87c660a95b445
Justin Su <3387175+injust@users.noreply.github.com>
2018-04-06 16:56:47 -0400

    Shorten a bit

--- fun-math/phantomc1/phantomc1.py
+++ fun-math/phantomc1/phantomc1.py
@@ -1,1 +1,1 @@
-print(lambda p:" ".join(([p.__setitem__(j,0)for j in p[i*i::i]],`i`+"*"*(p[i-2]|p[i+2]>1))[1]for i in p[2:]if p[i]))(range(input()))
+print" ".join(([p.__setitem__(j,0)for j in p[i*i::i]],`i`+"*"*(p[i-2]|p[i+2]>1))[1]for p in[range(input())]for i in p[2:]if p[i])
