c6c6b0232bad859d8214cc1e9e9b2b208ee3bd3a
Justin Su <3387175+injust@users.noreply.github.com>
2018-04-07 00:18:38 -0400

    Closer...

--- fun-math/phantomc1/phantomc1.py
+++ fun-math/phantomc1/phantomc1.py
@@ -1,1 +1,1 @@
-print" ".join(([p.__setitem__(j,0)for j in p[i*i::i]],`i`+"*"*(p[i-2]|p[i+2]>1))[1]for p in[range(input())]for i in p[2:]if p[i])
+print" ".join(`i`+([p.__setitem__(j,0)for j in p[i*i::i]]>p[i-2]|p[i+2]>0)*"*"for p in[range(input())]for i in p[2:]if p[i])
