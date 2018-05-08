aa813c9bf9d7391777c26ea17cc0484600951bc5
Justin Su <3387175+injust@users.noreply.github.com>
2018-05-08 07:39:28 -0400

    Update tle17c6p5

--- tle/tle17c6p5/tle17c6p5.py
+++ tle/tle17c6p5/tle17c6p5.py
@@ -2,10 +2,11 @@ def findRoots(at, cost):
 	if len(children[at]) == 1:
 		dest, add = children[at][0]
 		return findRoots(dest, cost + add)
-	global costBeforeTrees
-	costBeforeTrees += cost
+	global costNoTrees
+	costNoTrees += cost
 	if children[at]:
 		trees.append([at, 0, 0])
+		calcVisits(at)
 
 
 def calcVisits(at):
@@ -16,16 +17,13 @@ def calcVisits(at):
 
 
 def calcTreeCost(at):
-	cost = 0
-	for dest, add in children[at]:
-		cost += visits[dest] * add + calcTreeCost(dest)
-	return cost
+	return sum(visits[dest] * add + calcTreeCost(dest) for dest, add in children[at])
 
 
-def calcRootDistance(at=0):
+def calcRootDist(at=0):
 	for dest, add in children[at]:
-		rootDistance[dest] = rootDistance[at] + add
-		calcRootDistance(dest)
+		rootDist[dest] = rootDist[at] + add
+		calcRootDist(dest)
 
 
 def calcCanSave(at):
@@ -33,7 +31,7 @@ def calcCanSave(at):
 	delta = 0
 	for dest, cost in children[at]:
 		saveMain = (visits[dest] - 1) * parentCost[dest] + calcCanSave(dest)
-		saveAlt = max(0, saveMain - rootDistance[at])
+		saveAlt = max(0, saveMain - rootDist[at])
 		if saveMain - saveAlt > delta:
 			ret += saveMain - delta
 			delta = saveMain - saveAlt
@@ -51,17 +49,15 @@ for _ in xrange(n - 1):
 	u, v, t = map(int, input().split())
 	children[u - 1].append((v - 1, t))
 	parentCost[v - 1] = t
-costBeforeTrees = 0
+costNoTrees = 0
 trees = []
+visits = [0] * n
 for child in children[0]:
 	findRoots(*child)
-visits = [0] * n
-for root, _, _ in trees:
-	calcVisits(root)
 for tree in trees:
 	tree[1] = calcTreeCost(tree[0])
-rootDistance = [0] * n
-calcRootDistance()
+rootDist = [0] * n
+calcRootDist()
 for tree in trees:
 	tree[2] = calcCanSave(tree[0])
-print(costBeforeTrees + sum(treeCost - canSave for _, treeCost, canSave in trees))
+print(costNoTrees + sum(treeCost - canSave for _, treeCost, canSave in trees))
