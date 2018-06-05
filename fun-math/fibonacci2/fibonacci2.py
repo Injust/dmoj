fcf27dc8e6c8aab208e8f05e79ecbf12b93c5ab9
Justin Su <3387175+injust@users.noreply.github.com>
2018-06-05 05:44:46 -0400

    Solve fibonacci2

---
+++ fun-math/fibonacci2/fibonacci2.py
@@ -0,0 +1,7 @@
+def r(n):
+	f[n] = f[n] if n in f else (r(n >> 1) * r((n >> 1) + (n & 1)) + r((n >> 1) - (n & 1 ^ 1)) * r((n >> 1) - 1)) % 1000000007
+	return f[n]
+
+
+f = {0: 1, 1: 1}
+print(r(int(__import__("sys").stdin.read()) % 2000000016 - 1))
