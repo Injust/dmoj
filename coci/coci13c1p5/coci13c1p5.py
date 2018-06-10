cb8b6a71dca9f7f4a3928ba0b2f3ff5341b71d0c
Justin Su <3387175+injust@users.noreply.github.com>
2018-06-10 03:12:31 -0400

    Update coci13c1p5

--- coci/coci13c1p5/coci13c1p5.py
+++ coci/coci13c1p5/coci13c1p5.py
@@ -1,9 +1,3 @@
 freq = [0] * 2000000
 map(lambda size: freq.__setitem__(size - 1, freq[size - 1] + 1), map(int, __import__("sys").stdin.read().split()[1:]))
-out = 0
-for size in xrange(1, 2000001):
-	clubs = 0
-	for ind in xrange(size - 1, 2000000, size):
-		clubs += freq[ind]
-	out = max(out, (clubs > 1) * clubs * size)
-print(out)
+print(max((lambda clubs: (clubs > 1) * clubs * size)(sum(freq[ind] for ind in xrange(size - 1, 2000000, size))) for size in xrange(1, 2000001)))
