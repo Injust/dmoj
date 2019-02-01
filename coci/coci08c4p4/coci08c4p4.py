f5085b91fee9acd9419779f059b628a59aebc6eb
Justin Su <3387175+injust@users.noreply.github.com>
2019-02-01 01:05:39 -0500

    Update coci08c4p4.py

--- coci/coci08c4p4/coci08c4p4.py
+++ coci/coci08c4p4/coci08c4p4.py
@@ -1,7 +1,7 @@
 def recurse(x1, x2, y1, y2):
 	if x2 - x1 == 1:
 		out[y1][x1] = grid[y1][x1]
-		return grid[y1][x1] == "1", grid[y1][x1] == "0", 0
+		return grid[y1][x1] > "0", grid[y1][x1] < "1", 0
 	size = x2 - x1 >> 1
 	xmid = x1 + size
 	ymid = y1 + size
@@ -9,11 +9,11 @@ def recurse(x1, x2, y1, y2):
 	all0 = sum(square[0] for square in squares)
 	all1 = sum(square[1] for square in squares)
 	cost = 262144
-	for perm in __import__("itertools").permutations(xrange(4)):
-		newcost = squares[perm[0]][0] + squares[perm[1]][1] + squares[perm[2]][2] + squares[perm[3]][2]
+	for a, b, c, d in __import__("itertools").permutations(xrange(4)):
+		newcost = squares[a][0] + squares[b][1] + squares[c][2] + squares[d][2]
 		if newcost < cost:
 			cost = newcost
-			best = perm
+			best = a, b, c, d
 	for dy in xrange(size):
 		for dx in xrange(size):
 			out[y1 + size * (best[0] >> 1) + dy][x1 + size * (best[0] & 1) + dx] = "0"
